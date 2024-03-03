2271faec1ecfdc76f018629aba87960c7c76d309
FotiosBistas <fotis.bistas@gmail.com>
2024-03-04 01:37:34 +0200

    create links between hosts and switch

--- Topology.py
+++ Topology.py
@@ -57,5 +57,56 @@ class Topology(Topo):
             self.add_host(host_name=host_name, host_ip=host_ip)    
 
 
+    def add_direct_link_between_hosts(host_name1: str, host_name2: str, delay: str, bandwidth: int): 
+        def validate_delay(delay: str) -> bool:
+            # This regular expression matches strings that start with one or more digits followed by 'ms'
+            pattern = r'^\d+ms$'
+            if re.match(pattern, delay):
+                return True
+            else:
+                return False
+
+        if not isinstance(host_name1,str): 
+            raise ValueError("Error in add_direct_link_between_hosts: host_name1 should be a string.")
+
+        if not isinstance(host_name2,str):
+            raise ValueError("Error in add_direct_link_between_hosts:  host_name2 should be a string.")
+
+        if not isinstance(bandwidth, int): 
+            raise ValueError("Error in add_switch_link_for_host: bandwidth should be an integer.")
+
+        if not validate_delay(delay): 
+            raise ValueError("Error in add_switch_link_for_host: delay should be a string that follows this format 'Xms'")
+
+        self.addLink(self.hosts_dictionary[host_name1]['host_object'],self.hosts_dictionary[host_name2]['host_object'], delay=delay, bw=bandwidth)
+        
+
+    def add_switch_link_for_host(host_name:str, switch_name: str, delay: str, bandwidth: int):
+
+        def validate_delay(delay: str) -> bool:
+            # This regular expression matches strings that start with one or more digits followed by 'ms'
+            pattern = r'^\d+ms$'
+            if re.match(pattern, delay):
+                return True
+            else:
+                return False
+
+        if not isinstance(host_name,str): 
+            raise ValueError("Error in add_switch_link_for_host: host_name should be a string.")
+
+        if not isinstance(switch_name,str):
+            raise ValueError("Error in add_switch_link_for_host: switch_name should be a string.")
+
+        if not isinstance(bandwidth, int): 
+            raise ValueError("Error in add_switch_link_for_host: bandwidth should be an integer.")
+
+        if not validate_delay(delay): 
+            raise ValueError("Error in add_switch_link_for_host: delay should be a string that follows this format 'Xms'")
+
+        self.addLink(self.switches_dictionary[switch_name],self.hosts_dictionary[host_name]['host_object'], delay=delay, bw=bandwidth)
+
+        
+
+
 
 
