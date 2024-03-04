15030eee185347de08b88d7732e57e0674483265
FotiosBistas <fotis.bistas@gmail.com>
2024-03-04 02:24:00 +0200

    bug fixes: super init, interface corrections

--- Topology.py
+++ Topology.py
@@ -1,30 +1,37 @@
 import ipaddress
+import re
 
 from mininet.topo import Topo
+from mininet.log import setLogLevel, info
 
-class Topology(Topo): 
-    def __init__(self): 
+class CustomTopology(Topo): 
+    def __init__(self, log_level='info'): 
+        super().__init__()
         # This gets filled in add_host and will contain a structure: 
         #'host_name'= {
         #    'host_name': hosts_name, 
         #    'host_ip': host_ip, 
-        #    'host_object': h
         #}
         self.hosts_dictionary={}
 
         # This gets filled in add_switch and will contain a structure: 
-        # 'switch_name': 'switch_object'
+        # 'switch_name': 'switch_name'
         self.switches_dictionary={}
 
-    def add_switch(switch_name: str): 
+        setLogLevel(log_level)
+        
+
+    def add_switch(self, switch_name: str): 
 
         if not isinstance(switch_name,str): 
             raise ValueError("Error in add_switch: switch_name should be a string.")
 
-        new_switch=self.add_switch(switch_name)
-        self.switch[switch_name]=new_switch
+        new_switch=self.addSwitch(switch_name)
+        self.switches_dictionary[switch_name]={'switch_name': new_switch}
 
-    def add_host(host_name:str, host_ip:str):
+        info('Created switch: {switch_name}\n')
+
+    def add_host(self, host_name:str, host_ip:str):
 
         if not isinstance(host_name,str): 
             raise ValueError("Error in add_host: host_name should be a string.")
@@ -32,23 +39,21 @@ class Topology(Topo):
         if not isinstance(host_ip,str):
             raise ValueError("Error in add_host: host_ip should be a string.")
     
-        try:
-            ip = ipaddress.ip_address(host_ip)
-            print(f"{host_ip} is a valid IP address, while adding host.")
-        except ValueError:
-            print(f"Error: {host_ip} is not a valid IP address, while adding host.")
+        #try:
+        #    ip = ipaddress.ip_address(host_ip)
+        #    info(f"{host_ip} is a valid IP address, while adding host.\n")
+        #except ValueError:
+        #    info(f"Error: {host_ip} is not a valid IP address, while adding host.\n")
 
         h = self.addHost(host_name, ip=host_ip)
-        print(f"Added host: {host_name} with ip: {host_ip}")
+        info(f"Added host: {host_name} with ip: {host_ip}\n")
 
-        self.hosts_dictionary[host_name]=
-        {
-            'host_name': hosts_name, 
+        self.hosts_dictionary[host_name]={
+            'host_name': host_name, 
             'host_ip': host_ip, 
-            'host_object': h
         }
     
-    def add_hosts(hosts: dict): 
+    def add_hosts(self, hosts: dict): 
         
         if not isinstance(hosts, dict): 
             raise ValueError("Error in add_hosts. Hosts should be a python dictionary, with host_names as keys and host_ips as values.") 
@@ -57,7 +62,7 @@ class Topology(Topo):
             self.add_host(host_name=host_name, host_ip=host_ip)    
 
 
-    def add_direct_link_between_hosts(host_name1: str, host_name2: str, delay: str, bandwidth: int): 
+    def add_direct_link_between_hosts(self, host_name1: str, host_name2: str, delay: str=None, bandwidth: int=None): 
         def validate_delay(delay: str) -> bool:
             # This regular expression matches strings that start with one or more digits followed by 'ms'
             pattern = r'^\d+ms$'
@@ -72,16 +77,18 @@ class Topology(Topo):
         if not isinstance(host_name2,str):
             raise ValueError("Error in add_direct_link_between_hosts:  host_name2 should be a string.")
 
-        if not isinstance(bandwidth, int): 
-            raise ValueError("Error in add_switch_link_for_host: bandwidth should be an integer.")
+        if bandwidth is not None and not isinstance(bandwidth, int): 
+            raise ValueError("Error in add_direct_link_between_hosts: bandwidth should be an integer.")
 
-        if not validate_delay(delay): 
-            raise ValueError("Error in add_switch_link_for_host: delay should be a string that follows this format 'Xms'")
+        if delay is not None and not validate_delay(delay): 
+            raise ValueError("Error in add_direct_link_between_hosts: delay should be a string that follows this format 'Xms'")
 
-        self.addLink(self.hosts_dictionary[host_name1]['host_object'],self.hosts_dictionary[host_name2]['host_object'], delay=delay, bw=bandwidth)
+        self.addLink(self.hosts_dictionary[host_name1]['host_name'],self.hosts_dictionary[host_name2]['host_name'], delay=delay, bw=bandwidth)
+
+        info(f'Added link between host: {host_name1} and host {host_name2}, with delay {delay} and bandwidth {bandwidth}\n')
         
 
-    def add_switch_link_for_host(host_name:str, switch_name: str, delay: str, bandwidth: int):
+    def add_switch_link_for_host(self, host_name:str, switch_name: str, delay: str=None, bandwidth: int=None):
 
         def validate_delay(delay: str) -> bool:
             # This regular expression matches strings that start with one or more digits followed by 'ms'
@@ -97,13 +104,15 @@ class Topology(Topo):
         if not isinstance(switch_name,str):
             raise ValueError("Error in add_switch_link_for_host: switch_name should be a string.")
 
-        if not isinstance(bandwidth, int): 
+        if bandwidth is not None and not isinstance(bandwidth, int): 
             raise ValueError("Error in add_switch_link_for_host: bandwidth should be an integer.")
 
-        if not validate_delay(delay): 
+        if delay is not None and not validate_delay(delay): 
             raise ValueError("Error in add_switch_link_for_host: delay should be a string that follows this format 'Xms'")
 
-        self.addLink(self.switches_dictionary[switch_name],self.hosts_dictionary[host_name]['host_object'], delay=delay, bw=bandwidth)
+        self.addLink(self.switches_dictionary[switch_name]['switch_name'],self.hosts_dictionary[host_name]['host_name'], delay=delay, bw=bandwidth)
+
+        info(f'Added link between host: {host_name} and switch {switch_name}, with delay {delay} and bandwidth {bandwidth}\n')
 
         
 
--- snds.py
+++ snds.py
@@ -1,16 +1,47 @@
-from mininet.log import setLogLevel, info
+import os 
 
+from mininet.log import setLogLevel, info
 from minindn.minindn import Minindn
 from minindn.util import MiniNDNCLI
 from minindn.apps.app_manager import AppManager
 from minindn.apps.nfd import Nfd
 from minindn.apps.nlsr import Nlsr
+
 from time import sleep
-from mininet.topo import Topo
+from dotenv import load_dotenv
+
+# self made topology class
+from Topology import CustomTopology
+
+load_dotenv()
+
 
 def run():
     Minindn.cleanUp()
     Minindn.verifyDependencies()
+
+    topo = CustomTopology()
+
+    #Define hosts
+    hosts = {
+        'producer'  : os.getenv('PRODUCER_HOSTNAME'),
+        'forwarder' : os.getenv('FORWARDER_HOSTNAME'),
+        'consumer'  : os.getenv('CONSUMER_HOSTNAME'),
+        'closest'   : os.getenv('CLOSEST_HOSTNAME'),
+        'ngsild'    : os.getenv('NGSILD_HOSTNAME')       
+    }
+
+    info("Read hosts from environment variables\n")
+
+    topo.add_hosts(hosts)
+    topo.add_switch('switch1')
+
+    print(topo.hosts_dictionary)
+    print(topo.switches_dictionary)
+
+   
+    for host_name, _ in topo.hosts_dictionary.items(): 
+        topo.add_switch_link_for_host(host_name, 'switch1', delay='10ms')
 
     ndn = Minindn()
 
