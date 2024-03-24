11845d12fc2cadef31d8208019cd6dc3f9d8ce4d
FotiosBistas <fotis.bistas@gmail.com>
2024-03-24 08:29:54 +0200

    run through docker shell script

---
+++ run_through_docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+docker build -t snds .
+
+# Stop the container if it's running
+docker stop snds-app || true  # The `|| true` ensures that the script continues even if this command fails (e.g., if the container is not found).
+
+docker rm -f snds-app  # Remove the existing container if it exists
+
+docker run -m 4g --cpus=4 -it --privileged \
+  --env-file .env \
+  -v /lib/modules:/lib/modules \
+  --name snds-app \
+  snds \
+  /bin/bash
